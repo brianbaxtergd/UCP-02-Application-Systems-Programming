764e35b5b323c96713d019bd40a8a347bbe50dab
Brian Baxter <brianbaxtergd@gmail.com>
2019-10-08 12:02:26 -0500

    Challenge 1 Solutions.

---
+++ AsteraX UCP C02 V04 - Particle Effects Starter Project/Assets/__Scripts/OnlyEmitParticlesInBounds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class OnlyEmitParticlesInBounds : MonoBehaviour
+{
+    ParticleSystem.EmissionModule emitter;
+
+    void Start()
+    {
+        emitter = GetComponent<ParticleSystem>().emission;
+    }
+
+    void LateUpdate()
+    {
+        if (ScreenBounds.OOB(transform.position))
+        {
+            emitter.enabled = false;
+        }
+        else
+        {
+            emitter.enabled = true;
+        }
+    }
+}
